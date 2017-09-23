e1714d5b694803e11dda0abb973d622c755f2b90
danielBedoya <danielbedoyatbs@utp.edu.co>
2017-09-23 16:57:36 -0500

    defs faltantes

---
+++ def´sperl.py
@@ -0,0 +1,99 @@
+def t_RETURN(t):
+	r'return'
+	return t
+
+def t_RMDIR(t):
+	r'rmdir'
+	return t
+
+def t_SEEK(t):
+	r'seek'
+	return t
+
+def t_SELECT(t):
+	r'select'
+	return t
+
+def t_SHIFT(t):
+	r'shift'
+	return t
+
+def t_SIN(t):
+	r'sin'
+	return t
+
+def t_SLEEP(t):
+	r'sleep'
+	return t
+
+def t_SORT(t):
+	r'sort'
+	return t
+
+def t_SPLIT(t):
+	r'split'
+	return t
+
+def t_SQRT(t):
+	r'sqrt'
+	return t
+
+def t_SYSTEM(t):
+	r'system'
+	return t
+
+def t_TELL(t):
+	r'tell'
+	return t
+
+def t_VALUES(t):
+	r'values'
+	return t
+
+def t_WRITE(t):
+	r'write'
+	return t
+
+def t_IF(t):
+	r'if'
+	return t
+
+def t_UNLESS(t):
+	r'unless'
+	return t
+
+def t_ELSE(t):
+	r'else'
+	return t
+
+def t_ELSIF(t):
+	r'elsif'
+	return t
+
+def t_NEXT(t):
+	r'next'
+	return t
+
+def t_LAST(t):
+	r'last'
+	return t
+
+def t_FOR(t):
+	r'for'
+	return t
+
+def t_WHILE(t):
+	r'while'
+	return t
+
+def t_FOREACH(t):
+	r'foreach'
+	return t
+
+def t_DO(t):
+	r'do'
+	return t
+
+def t_UNTIL(t):
+	r'until'
+	return t
